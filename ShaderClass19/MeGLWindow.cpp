39251d028726b7bb0ec831b7cd4938ce6511c6c9
Liz <lizivey95@gmail.com>
2019-09-26 15:37:00 -0600

    shape update

--- ShaderClass19/MeGLWindow.cpp
+++ ShaderClass19/MeGLWindow.cpp
@@ -34,7 +34,7 @@ namespace
 {
 	Vertex Ball[] =
 	{
-		glm::vec3(0.0f, 0.1f, 0.0f),//0
+		/*glm::vec3(0.0f, 0.1f, 0.0f),//0
 		glm::vec3(0.0f, 1.0f, 0.0f),
 
 		glm::vec3(0.1f, 0.0f, 0.0f),//1
@@ -50,6 +50,21 @@ namespace
 		glm::vec3(0.0f, 1.0f, 0.0f),
 
 		glm::vec3(-0.2f, 0.0f, 0.0f),//5
+		glm::vec3(0.0f, 1.0f, 0.0f),*/
+
+		glm::vec3(0.0f, 0.2f, 0.0f),//0
+		glm::vec3(0.0f, 1.0f, 0.0f),
+
+		glm::vec3(-0.1f, 0.1f, 0.0f),//0
+		glm::vec3(0.0f, 1.0f, 0.0f),
+
+		glm::vec3(-0.1f, -0.1f, 0.0f),//0
+		glm::vec3(0.0f, 1.0f, 0.0f),
+
+		glm::vec3(0.1f, -0.1f, 0.0f),//0
+		glm::vec3(0.0f, 1.0f, 0.0f),
+
+		glm::vec3(0.1f, 0.1f, 0.0f),//0
 		glm::vec3(0.0f, 1.0f, 0.0f),
 	};
 
